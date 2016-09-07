d46c148954e551c460b96a8b5bcc4239b40f89d5
Sam Ingle <sam@untoldhq.com>
2016-09-06 20:26:28 -0700

    Initial Commit

---
+++ status-board/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  status-board
+//
+//  Created by Sam Ingle on 9/6/16.
+//  Copyright © 2016 Untold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
